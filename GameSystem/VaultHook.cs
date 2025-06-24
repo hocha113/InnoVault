9bbc0488f64f04a56efd901205e0f096090cc81b
HoCha113 <2188704314@qq.com>
2025-06-24 19:10:15 +0800

    提交物品重制节点

---
+++ GameSystem/VaultHook.cs
@@ -0,0 +1,76 @@
+﻿using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace InnoVault.GameSystem
+{
+    /// <summary>
+    /// 用于挂载和存储钩子实例
+    /// </summary>
+    public class VaultHook : IVaultLoader
+    {
+        private static ConcurrentDictionary<(MethodBase, Delegate), Hook> _hooks = new ConcurrentDictionary<(MethodBase, Delegate), Hook>();
+        /// <summary>
+        /// 缓存的被添加过的钩子实例，目前没有其他作用，仅仅是延长生命周期
+        /// </summary>
+        public static ConcurrentDictionary<(MethodBase, Delegate), Hook> Hooks => _hooks;
+
+        /// <summary>
+        /// 添加钩子到指定方法
+        /// </summary>
+        /// <param name="method">要钩住的方法</param>
+        /// <param name="hookDelegate">钩子委托</param>
+        /// <returns>创建的Hook对象</returns>
+        public static Hook Add(MethodBase method, Delegate hookDelegate) {
+            if (method == null) {
+                throw new ArgumentException("The MethodBase passed in is Null");
+            }
+            if (hookDelegate == null) {
+                throw new ArgumentException("The HookDelegate passed in is Null");
+            }
+
+            Hook hook = new Hook(method, hookDelegate);
+
+            if (!hook.IsApplied) {
+                hook.Apply();
+            }
+            if (!_hooks.TryAdd((method, hookDelegate), hook)) {
+                string ctext = "目标方法已经被该委托挂载";
+                string egtext = "The target method is already mounted by the delegate";
+                VaultMod.Instance.Logger.Info(VaultUtils.Translation(ctext, egtext));
+            }
+            return hook;
+        }
+
+        /// <summary>
+        /// 检测钩子的挂载状态，如果没有任何异常将返回<see langword="true"/>，否则返回<see langword="false"/>
+        /// </summary>
+        /// <returns></returns>
+        public static bool CheckHookStatus(out int hookDownNum) {
+            hookDownNum = 0;
+
+            foreach (var hook in _hooks.Values) {
+                if (!hook.IsApplied) {
+                    VaultMod.Instance.Logger.Info((hook + VaultUtils.Translation("挂载失效", "Mount failure")));
+                    hookDownNum++;
+                }
+            }
+
+            return hookDownNum == 0;
+        }
+
+        void IVaultLoader.UnLoadData() {
+            foreach (var hook in _hooks.Values) {
+                if (hook == null) {
+                    continue;
+                }
+                if (hook.IsApplied) {
+                    hook.Undo();
+                }
+                hook.Dispose();
+            }
+            _hooks.Clear();
+        }
+    }
+}
--- VaultUtils.cs
+++ VaultUtils.cs
@@ -1000,6 +1000,31 @@ namespace InnoVault
         }
 
         /// <summary>
+        /// 获取指定物品的本地化描述
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static LocalizedText GetLocalizedItemTooltip<T>() where T : ModItem => GetLocalizedItemTooltip(ModContent.ItemType<T>());
+
+        /// <summary>
+        /// 获取指定物品的本地化描述
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static LocalizedText GetLocalizedItemTooltip(int id) {
+            if (id <= ItemID.None) {
+                return null;
+            }
+
+            if (id < ItemID.Count) {
+                return Language.GetText("ItemTooltip." + ItemID.Search.GetName(id));
+            }
+            else {
+                return ItemLoader.GetItem(id).GetLocalization("Tooltip");
+            }
+        }
+
+        /// <summary>
         /// 解析字符串键并获取对应的物品类型
         /// </summary>
         /// <param name="fullName">用于解析的字符串键，可以是整数类型或模组/物品名称的组合</param>
