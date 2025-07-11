8751b15e63aaba1f20374b3730179c8e8d596cd6
HoCha113 <2188704314@qq.com>
2025-07-12 03:24:01 +0800

    完善NBT存档

--- GameSystem/SaveMod.cs
+++ GameSystem/SaveMod.cs
@@ -6,6 +6,7 @@ namespace InnoVault.GameSystem
     /// <summary>
     /// 管理模组全局数据的基类，加载读取范围从游戏加载末期开始覆盖全程
     /// 一般用于进行标题界面或者菜单界面等游戏世界外的数据存档
+    /// 该基类并不会自动调用保存或者读取逻辑，需要自行在何时的时机中去调用DoLoad或者DoSave存取需要的数据
     /// </summary>
     public class SaveMod : SaveContent<SaveMod>
     {
