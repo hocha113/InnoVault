0ea006b7de71c45776e44264920e22cc7811aee6
HoCha113 <2188704314@qq.com>
2025-07-14 17:39:32 +0800

    添加多国语言

--- GameSystem/ItemOverride.cs
+++ GameSystem/ItemOverride.cs
@@ -67,7 +67,7 @@ namespace InnoVault.GameSystem
         /// 是否参与配方替换，默认为<see langword="true"/>，如果<see cref="IsVanilla"/>为<see langword="true"/>，那么该属性自动返回<see langword="false"/>
         /// [未使用]
         /// </summary>
-        [Obsolete]
+        [Obsolete("该属性已经弃用，不再具备任何实际用途，如果需要检测配方合成，请直接判定ID或者使用IsVanilla")]
         public virtual bool FormulaSubstitution => !IsVanilla;
         /// <summary>
         /// 该重置节点是否会加载进图鉴中，默认为<see langword="true"/>
